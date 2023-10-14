6e63faf380d29ad9dcb630cdd249b809b06e8f6b
Lokman Hakim <142791901+lokmanTech@users.noreply.github.com>
2023-10-14 20:56:53 +0800

    Create experiments.js

---
+++ 06 JavaScript Experiment/js/experiments.js
@@ -0,0 +1,20 @@
+// First code = to show the result on the console
+//const monthlyRent = 500;
+//const yearlyRent = monthlyRent * 12;
+//console.log(yearlyRent);
+
+
+// Second Code
+//const monthlyRent = 500;
+//const yearlyRent = monthlyRent * 12;
+// Find the HTML element with the id "result"
+//const resultElement = document.getElementById("result");
+// Update the content of the result element
+//resultElement.textContent = 'Yearly Rent: RM ' + yearlyRent;
+
+const firstName = "Lokman";
+const lastName = "Hakim";
+
+const sentence = `Hello ${firstName} ${lastName}! How are you!?`;
+
+console.log(sentence);
